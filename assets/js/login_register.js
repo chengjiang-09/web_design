f8f090ef3de02d9e03a74cf3e8b3100f18b25f32
chengjiang_09 <chengjiang_09@163.com>
2021-12-15 11:11:22 +0800

    完成注册模块

--- assets/js/login_register.js
+++ assets/js/login_register.js
@@ -7,15 +7,47 @@ class register{
         this.init()
     }
     init(){
-        this.register_submit()
-        this.verify_data()
+        this.register_submit() //注册提交
+        this.verify_data()  //表单验证
     }
     register_submit(){
-        this.register.on("submit",function(e){
+        this.register.on("submit",(e) => {
             e.preventDefault()
+
+            let inputs = this.register.find("input")
+            let flag = true
+
+            inputs.each((index,dom) => {
+                $(dom).val() === "" ? flag = false : flag = true
+            })
+
+            if(flag){
+                let phone = this.inputPhone.val()
+                let password1 = this.inputPassword1.val()
+                let password2 = this.inputPassword2.val()
+
+                $.ajax({
+                    type:"POST",
+                    url:"http://127.0.0.1/web_bookstore/user_register",
+                    data:{
+                        phone:phone,
+                        password1:password1,
+                        password2:password2,
+                    },
+                    success:function(res){
+                        if(res.status === 0){
+                            localStorage.setItem("phone_Cookie",res.datas.phone)
+                            location.href = "./index.html"
+                        }else if(res.status === 1){
+                            alert(res.msg)
+                        }
+                    }
+                })
+            }
         })
     }
     verify_data(){
+        //手机号验证
         this.inputPhone.on("blur",function(){
 
             let str = $(this).val()
@@ -27,13 +59,69 @@ class register{
 
                 if(str.match(re)){
                     $(this).parent().siblings(".warning").children("p").html("")
+
+                    $.ajax({
+                        type:"POST",
+                        url:"http://127.0.0.1/api/verification_phone",
+                        data:{
+                            phone:str
+                        },
+                        success:function(res){
+                            if(res.status === 0){
+                                $(this).parent().siblings(".warning").children("p").html("")
+                            }else if(res.status === 1){
+                                $(this).parent().siblings(".warning").children("p").html(data.msg)
+                            }
+                        }
+                    })
+
+
                 }else{
                     $(this).parent().siblings(".warning").children("p").html("手机号输入错误！")
                 }
             }
         })
 
+        //密码验证
+        this.inputPassword1.on("blur",function(){
+
+            let str = $(this).val()
+
+            if(str.trim() === ""){
+
+                $(this).parent().siblings(".warning").children("p").html("密码格式错误！")
+
+            }else{
+                let re = /^[0-9a-zA-Z_-]{6,20}$/
+
+                if(str.match(re)){
+                    $(this).parent().siblings(".warning").children("p").html("")
+                }else{
+                    $(this).parent().siblings(".warning").children("p").html("密码格式错误！")
+                }
+            }
+
+        })
         
+        //二次密码验证
+        this.inputPassword2.on("blur",() => {
+
+            let str = this.inputPassword2.val()
+
+            if(str.trim() === ""){
+
+                this.inputPassword2.parent().siblings(".warning").children("p").html("密码输入不一致！")
+
+            }else{
+                
+                if(str === this.inputPassword1.val()){
+                    this.inputPassword2.parent().siblings(".warning").children("p").html("")
+                }else{
+                    this.inputPassword2.parent().siblings(".warning").children("p").html("密码输入不一致！")
+                }
+            }
+
+        })
     }
 }
 $(function(){
