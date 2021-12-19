336ad2e49d2afea256263d38bdeb34324338da2e
chengjiang_09 <chengjiang_09@163.com>
2021-12-19 19:03:52 +0800

    主页初始化1.0

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,38 @@
 class init_index{
     constructor(){
         this.bookAside = $(".bookAside")
-        this.
+        this.init()
+        this.bookList = []
+    }
+    async init(){
+        await $.ajax({
+            type:"GET",
+            url:"http://127.0.0.1/web_bookstore/init_searchbook",
+            data:{},
+            success:(res) => {
+                if(res.status == 0){
+                    this.bookList = res.datas
+                }else if(res.status == 1){
+                    console.log("出错啦！");
+                }
+            }
+        })
+        
+        this.init_bookAside()
+        this.init_bookBox()
+    }
+    init_bookAside(){
+        let bookAsideStr = util_template(this.bookList,"bookAside")
+        this.bookAside.find("ul").html(bookAsideStr)
+    }
+    init_bookBox(){
+        let bookList = util_template(this.bookList,"oneBook",true)
+
+        $(".commonBook").find(".bookBox").html(bookList[2])
+        $(".newBook").find(".bookBox").html(bookList[0])
+        $(".activeBook").find(".bookBox").html(bookList[1])
     }
 }
+$(function(){
+    new init_index()
+})
--- assets/utils/util_template.js
+++ assets/utils/util_template.js
@@ -1,22 +1,45 @@
-const util_template = (datas, id) => {
+const util_template = (datas, id, index) => {
 
     let template = $(`#${id}`)
 
     let re = /{{\s*([a-zA-Z0-9_-]+)\s*}}/
 
-    let str = ""
+    if (index) {
+        let newBookStr = ""
+        let activeBookStr = ""
+        let commonBookStr = ""
 
-    $.each(datas, function (index, obj) {
-        let templateStr = template.html()
-        let dataStr = ""
+        $.each(datas, function (index, obj) {
+            if (obj.bookstatus == 0) {
+                newBookStr += init_template(template,obj,re)
+            } else if (obj.bookstatus == 1) {
+                activeBookStr += init_template(template,obj,re)
+            } else if (obj.bookstatus == 2) {
+                commonBookStr += init_template(template,obj,re)
+            }
+        })
 
-        while (dataStr = re.exec(templateStr)) {
-            templateStr = templateStr.replace(dataStr[0], obj[dataStr[1]])
-        }
+        return [newBookStr,activeBookStr,commonBookStr]
+    } else {
+        let str = ""
 
-        str += templateStr
-    })
+        $.each(datas, function (index, obj) {
+            str += init_template(template,obj,re)
+        })
 
-    return str
+        return str
+    }
+}
+
+const init_template = (template,obj,re) => {
+
+    let templateStr = template.html()
+    let dataStr = ""
+
+    while (dataStr = re.exec(templateStr)) {
+        templateStr = templateStr.replace(dataStr[0], obj[dataStr[1]])
+    }
+
+    return templateStr
 }
 
